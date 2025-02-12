7fe18b344c8aaae5f635877e26a6f0c69e06ee73
Victor <victor.meyo@student.moringaschool.com>
2025-02-12 09:06:04 +0300

    First Attempt

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id = "about" >
+    <h2>About Me</h2>
+    <p>I am a special coder</p>
+    <img src = {image} alt = "I painted this" />
+    About</div>;
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>NavBar
+    <a id = "home"> Home</a>
+    <a id = "about">About </a>
+  </nav>;
 }
 
 export default NavBar;
